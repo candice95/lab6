0e6967d05f5f06371ee1559e1fbdcd1e84d2b801
vagrant <vagrant@precise32.(none)>
2015-02-12 19:58:39 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -12,6 +12,8 @@ function initializePage() {
 	$('.project a').click(addProjectDetails);
 
 	$('#colorBtn').click(randomizeColors);
+
+	$('#worldBankBtn').click(showWorldBank);
 }
 
 /*
@@ -27,6 +29,17 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+		
+	$.get("/project/"+idNumber, addDetail);
+	console.log("/project/"+idNumber);
+}
+
+function addDetail(result) {
+	console.log(result);
+	// Get the correct id
+	var projID = result['id'];
+	var projectHTML = '<img src="' + result['image'] + '"class="detailsImage">' + '<h3>' + result['date'] + '</h3>' + '<h4>' + result['summary'] + '</h4>'
+	$("#project"+projID).html(projectHTML);
 }
 
 /*
@@ -35,4 +48,28 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+	$.get("/palette", changeColor);
 }
+
+function changeColor(result) {
+	var color = result['colors'];
+	var colors = color['hex'];
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
+}
+
+function showWorldBank(e) {
+	console.log("User clicked on worldBank button");
+	$.get('http://api.worldbank.org/countries/all/indicators/SP.POP.TOTL?format=json', callbackFunction, 'jsonp');
+}
+
+function callbackFunction( result ) {
+	console.log(result);
+	var country = result['country'];
+	var value = country['value'];
+	$('#worldBankBtn').append(value);
+}
+
